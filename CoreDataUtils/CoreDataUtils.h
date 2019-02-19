7f001f37c0264d189a71021d2d53b1ce98a2008e
Renzo Manuel Alvarado Passalacqua <ralvarado@mac-mini-de-ralvarado.local>
2019-02-19 11:43:49 -0500

    Initial Commit

---
+++ CoreDataUtils/CoreDataUtils.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataUtils.h
+//  CoreDataUtils
+//
+//  Created by Renzo Manuel Alvarado Passalacqua on 2/19/19.
+//  Copyright © 2019 Belatrixsf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataUtils.
+FOUNDATION_EXPORT double CoreDataUtilsVersionNumber;
+
+//! Project version string for CoreDataUtils.
+FOUNDATION_EXPORT const unsigned char CoreDataUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataUtils/PublicHeader.h>
+
+
